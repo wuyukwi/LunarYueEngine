d92f08c22862d3bd2f38190b035f869a631fb8dd
wuyukwi <b1044763336@gmail.com>
2023-07-27 13:45:24 +0900

    updata

--- source/editor/editor_runtime/main.cpp
+++ source/editor/editor_runtime/main.cpp
@@ -10,9 +10,8 @@ int main(int argc, char* argv[])
     fs::path engine_path         = fs::absolute(fs::path(ENGINE_DIRECTORY));
     fs::path shader_include_path = fs::absolute(fs::path(SHADER_INCLUDE_DIRECTORY));
 
-    const fs::path engine      = engine_path / "data/engine_data";
-    const fs::path editor      = engine_path / "data/editor_data";
-    fs::path       binary_path = fs::executable_path(argv[0]).parent_path();
+    const fs::path engine = engine_path / "data/engine_data";
+    const fs::path editor = engine_path / "data/editor_data";
     fs::add_path_protocol("engine:", engine);
     fs::add_path_protocol("editor:", editor);
     fs::add_path_protocol("binary:", binary_path);
